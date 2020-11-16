472fcf2b26dda0c3c415df7a3bd7fa9cbd23e12d
vlad <vladov3000@gmail.com>
2020-11-15 20:05:27 -0800

    fixed play gif

--- src/play.rs
+++ src/play.rs
@@ -8,6 +8,6 @@ pub fn play_gif(file: File) {
     options.set_color_output(gif::ColorOutput::RGBA);
     let mut decoder = options.read_info(file).unwrap();
     while let Some(frame) = decoder.read_next_frame().unwrap() {
-        println!("{:?}", frame);
+        println!("{} {}", frame.height, frame.width);
     }
 }
