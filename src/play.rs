8bfec07e555fa871223087729ef7ecad59bbd395
vlad <vladov3000@gmail.com>
2020-11-15 19:56:03 -0800

    play gif

--- src/play.rs
+++ src/play.rs
@@ -1,6 +1,13 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use gif;
 
 pub fn play_gif(file: File) {
     println!("Playing gif...");
+    let mut options = gif::DecodeOptions::new();
+    options.set_color_output(gif::ColorOutput::RGBA);
+    let mut decoder = options.read_info(file).unwrap();
+    while let Some(frame) = decoder.read_next_frame().unwrap() {
+        println!("{:?}", frame);
+    }
 }
